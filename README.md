42143d3db84092d30be5f92605ab8e8c80ada9c6
janschiefer <janschiefer@users.noreply.github.com>
2022-11-14 20:09:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-starter-useFetch-BUG
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-d5y9os)
---
+++ app.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+async function testFetchContentType() {
+		
+	
+	let add_to_cart_data = {
+			product_id: "123",
+			item_quantity: "10",
+			
+	};
+	
+	await useFetch(() => `/addToCart`, {
+        method: 'POST',
+        mode: 'no-cors',
+    	headers: {
+    		"content-type": "application/json"
+    	},
+        body: add_to_cart_data,
+        initialCache: false,
+        
+        onResponse({ request, response, options }) {
+    	    // Process the response data
+    	    
+    	   window.console.error("Add to cart... ");	    	 
+
+
+    	},
+ 
+    	
+    } );
+
+	
+}
+
+</script>
+
+<template>
+
+	<p> Check network tab in web developer tools...</p>
+	
+  	 <button  @click="testFetchContentType()" >TEST FETCH</button>
+	
+
+</template>
